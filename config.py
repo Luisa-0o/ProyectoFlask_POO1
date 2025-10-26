e1e73c0b4febe8be71231518359475014ddda22d
Luisa <luisa.builes2666@unaula.edu.co>
2025-10-26 14:56:25 -0500

    fix:connection string

--- config.py
+++ config.py
@@ -3,5 +3,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'cambia-esta-clave-por-una-segura'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:1234@localhost/ecommerse'
+    SQLALCHEMY_DATABASE_URI = (
+        os.getenv('DATABASE_URL') or
+        'postgresql+psycopg2://postgres:1234@localhost/ecommerse'
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+    
