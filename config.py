d5289b6e6828c4cfebc77386665d13a9143e7516
Luisa <luisa.builes2666@unaula.edu.co>
2025-10-26 15:27:28 -0500

    Correcciones

--- config.py
+++ config.py
@@ -1,12 +1,19 @@
+# import os
+# basedir = os.path.abspath(os.path.dirname(__file__))
+
+# class Config:
+#     SECRET_KEY = os.environ.get('SECRET_KEY') or 'cambia-esta-clave-por-una-segura'
+#     SQLALCHEMY_DATABASE_URI = (
+#         os.getenv('DATABASE_URL') or
+#         'postgresql+psycopg2://postgres:1234@localhost/ecommerse'
+#     )
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 import os
-basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'cambia-esta-clave-por-una-segura'
-    SQLALCHEMY_DATABASE_URI = (
-        os.getenv('DATABASE_URL') or
-        'postgresql+psycopg2://postgres:1234@localhost/ecommerse'
-    )
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'clave-segura'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
